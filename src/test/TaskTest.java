ed7fa82d279aad2095459f6866a35b50a791e080
Katrin-Seagull <D1stiel@yandex.ru>
2025-07-26 15:57:35 +0300

    Второй коммит ветки

--- src/test/TaskTest.java
+++ src/test/TaskTest.java
@@ -4,7 +4,6 @@ import model.Task;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import manager.InMemoryTaskManager;
-import java.util.List;
 
 class TaskTest {
     InMemoryTaskManager taskManager = new InMemoryTaskManager();
